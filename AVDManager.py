6082649d4669385c7c4508f03dc22faba327773f
Alfred Shi <memoriedsea@gmail.com>
2015-01-30 17:34:22 +0800

    add modify imsi/iemi function

--- AVDManager.py
+++ AVDManager.py
@@ -1,21 +1,38 @@
 __author__ = 'liashi'
 
 import argparse
+import ConfigParser
+from emulator_writer import emulator_writer
+import shutil
 
-def get_args():
-    pass
+def copy_emulator_source():
 
-def write_emu_binary():
-    pass
-
-def overwrite_build_prop():
     pass
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="AVD Manager")
-    parser.add_argument("-c", "--create", help="Create specific AVD.")
+    parser.add_argument("-e", "--iemi", help="The iemi string you want to write into the emulator")
+    parser.add_argument("-m", "--imsi", help="The imsi string you want to write into the emulator")
+    parser.add_argument("-b", "--buildprop", help="The build.prop path.")
 
     args = parser.parse_args()
 
-    if args.create:
+    configParser = ConfigParser.ConfigParser()
+    ConfigParser.optionxform = str
+    configParser.read("AVDManager.properites")
+
+    emulator_name = configParser.get("sdk", "emulator_path")[configParser.get("sdk", "emulator_path").rindex("/") + 1:]
+
+    system_img_name = configParser.get("sdk", "system_img_path")[configParser.get("sdk", "system_img_path").rindex("/") + 1:]
+
+    shutil.copyfile(configParser.get("sdk", "emulator_path"), "./output/" + emulator_name)
+
+    shutil.copyfile(configParser.get("sdk", "system_img_path"), "./output" + system_img_name)
+
+
+    if args.iemi:
+        emulator_writer.write_iemi("./output/" + emulator_name, "123456789012345")
+    if args.imsi:
+        emulator_writer.write_imsi("./output/" + emulator_name, "310260123456789")
+    if args.buildprop:
         pass
