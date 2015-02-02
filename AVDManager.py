14b6a90c8508c9c14d13f6f308cca21cbae195e0
Alfred Shi <memoriedsea@gmail.com>
2015-02-02 17:28:31 +0800

    fix bug & modify output file name

--- AVDManager.py
+++ AVDManager.py
@@ -30,18 +30,23 @@ if __name__ == "__main__":
 
     if args.iemi or args.imsi:
         emulator_name = configParser.get("sdk", "emulator_path")[configParser.get("sdk", "emulator_path").rindex(os.sep) + 1:]
+        emulator_name_after = ""
 
         shutil.copyfile(configParser.get("sdk", "emulator_path"), emulator_name)
 
-        if args.iemi:
-            emulator_writer.write_iemi(emulator_name, args.iemi)
-        if args.imsi:
-            emulator_writer.write_imsi(emulator_name, args.imsi)
-
         if platform.system() == "Windows":
-            shutil.move(emulator_name, configParser.get("sdk", "emulator_path")[0: configParser.get("sdk", "emulator_path").rindex(".")] + "_" + str(args.port) + ".exe")
+            emulator_name_after = emulator_name[0: emulator_name.index("-")] + "-" + args.port + ".exe"
         elif platform.system() == "Linux":
-            shutil.move(emulator_name, configParser.get("sdk", "emulator_path") + "_" + args.port)
+            emulator_name_after = emulator_name[0: emulator_name.index("-")]  + "-" + args.port
+
+        shutil.move(emulator_name, emulator_name_after)
+
+        if args.iemi:
+            emulator_writer.write_iemi(emulator_name_after, args.iemi)
+        if args.imsi:
+            emulator_writer.write_imsi(emulator_name_after, args.imsi)
+
+        shutil.move(emulator_name_after, configParser.get("sdk", "emulator_path")[0:configParser.get("sdk", "emulator_path").rindex(os.sep) + 1] + emulator_name_after)
     #if args.buildprop:
         #print platform.architecture()
         #extract the img file
