d47741e6b248526532fbc8768e1ffd3717786c97
Alfred Shi <memoriedsea@gmail.com>
2015-02-02 14:41:15 +0800

    Merge windows/linux

--- AVDManager.py
+++ AVDManager.py
@@ -13,9 +13,11 @@ def copy_emulator_source():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="AVD Manager")
-    parser.add_argument("-e", "--iemi", help="The iemi string you want to write into the emulator")
-    parser.add_argument("-m", "--imsi", help="The imsi string you want to write into the emulator")
+    parser.add_argument("-e", "--iemi", help="The iemi string you want to write into the emulator.")
+    parser.add_argument("-m", "--imsi", help="The imsi string you want to write into the emulator.")
+    parser.add_argument("-p", "--port", help="The port AVD use.")
     parser.add_argument("-b", "--buildprop", help="The build.prop path.")
+    parser.add_argument("-o", "--output", help="Output path for system.img.")
 
     args = parser.parse_args()
 
@@ -23,24 +25,27 @@ if __name__ == "__main__":
     ConfigParser.optionxform = str
     configParser.read("AVDManager.properites")
 
-    emulator_name = configParser.get("sdk", "emulator_path")[configParser.get("sdk", "emulator_path").rindex("/") + 1:]
+    #system_img_name = configParser.get("sdk", "system_img_path")[configParser.get("sdk", "system_img_path").rindex(os.sep) + 1:]
+    #shutil.copyfile(configParser.get("sdk", "system_img_path"), "./output/" + system_img_name)
 
-    system_img_name = configParser.get("sdk", "system_img_path")[configParser.get("sdk", "system_img_path").rindex("/") + 1:]
+    if args.iemi or args.imsi:
+        emulator_name = configParser.get("sdk", "emulator_path")[configParser.get("sdk", "emulator_path").rindex(os.sep) + 1:]
 
-    shutil.copyfile(configParser.get("sdk", "emulator_path"), "./output/" + emulator_name)
+        shutil.copyfile(configParser.get("sdk", "emulator_path"), emulator_name)
 
-    shutil.copyfile(configParser.get("sdk", "system_img_path"), "./output/" + system_img_name)
+        if args.iemi:
+            emulator_writer.write_iemi(emulator_name, args.iemi)
+        if args.imsi:
+            emulator_writer.write_imsi(emulator_name, args.imsi)
 
-    print platform.system()
-
-    if args.iemi:
-        emulator_writer.write_iemi("./output/" + emulator_name, "123456789012345")
-    if args.imsi:
-        emulator_writer.write_imsi("./output/" + emulator_name, "310260123456789")
-    if args.buildprop:
+        if platform.system() == "Windows":
+            shutil.move(emulator_name, configParser.get("sdk", "emulator_path")[0: configParser.get("sdk", "emulator_path").rindex(".")] + "_" + str(args.port) + ".exe")
+        elif platform.system() == "Linux":
+            shutil.move(emulator_name, configParser.get("sdk", "emulator_path") + "_" + args.port)
+    #if args.buildprop:
         #print platform.architecture()
         #extract the img file
-        os.system("./libs/yaffs2utils/unyaffs2 -f " + "./output/" + system_img_name)
+        #os.system("./libs/yaffs2utils/unyaffs2 -f " + "./output/" + system_img_name)
 
         #overwrite the file
 
